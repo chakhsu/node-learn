331c36edfc2f6d646f5547bcaeb382b734e84c4c
Chakhsu Lau <zexu.liu@live.cn>
2017-04-24 03:06:30 +0800

    update

---
+++ express-demo/index.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/users');
+
+//设置存放模板文件的目录位置
+app.set('views', path.join(__dirname,'views'));
+//设置模板引擎为 ejs
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
+app.listen(8081);
