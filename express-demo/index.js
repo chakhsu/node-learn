bc16972422893a21a553dbc18c4864217d6076d2
Chakhsu Lau <zexu.liu@live.cn>
2017-04-24 04:15:57 +0800

    update

--- express-demo/index.js
+++ express-demo/index.js
@@ -13,3 +13,18 @@ app.use('/', indexRouter);
 app.use('/users', userRouter);
 
 app.listen(8081);
+
+// var express = require('express');
+// var app = express();
+
+// app.use(function(req,res,next){
+//   console.log('1');
+//   next();
+// });
+
+// app.use(function(req,res,next){
+//   console.log('2');
+//   res.status(200).end();
+// });
+
+// app.listen(8081);
